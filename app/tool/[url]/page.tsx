b1abf0265946086f69b7f970d5afb4133dfc84e9
Liam Aboros <sharon.mishayev@gmail.com>
2025-02-28 10:48:40 +0200

    added revalidate to see new tools

--- app/tool/[url]/page.tsx
+++ app/tool/[url]/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 60; // Update every 60 seconds
+
 import { Metadata, ResolvingMetadata } from "next";
 import { notFound } from "next/navigation";
 import Image from "next/image";
